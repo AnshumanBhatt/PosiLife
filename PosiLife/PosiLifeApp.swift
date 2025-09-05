489c03d4a68877dfe5dcae33c724776a0dbbe91e
Anshuman Bhatt <94911238+AnshumanBhatt@users.noreply.github.com>
2025-09-05 19:47:15 +0530

    Initial Commit

---
+++ PosiLife/PosiLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosiLifeApp.swift
+//  PosiLife
+//
+//  Created by Anshuman Bhatt on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PosiLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
